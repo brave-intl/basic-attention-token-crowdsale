c09f2d25f986b1e0b74d058d50c88fb2275a7cf1
nfnty <git@nfnty.se>
2017-05-26 01:58:18 +0200

    contracts: `createTokens`: Reduce nesting

--- contracts/BAToken.sol
+++ contracts/BAToken.sol
@@ -51,15 +51,16 @@ contract BAToken is StandardToken, SafeMath {
       if (block.number < fundingStartBlock) throw;
       if (block.number > fundingEndBlock) throw;
       if (msg.value == 0) throw;
+
       uint256 tokens = safeMult(msg.value, tokenExchangeRate); // check that we're not over totals
       uint256 checkedSupply = safeAdd(totalSupply, tokens);
-      if(tokenCreationCap < checkedSupply) {    // odd fractions won't be found
-        throw;                           // they need to get their money back if something goes wrong
-      } else {
-        totalSupply = checkedSupply;
-        balances[msg.sender] += tokens;   // safeAdd not needed; bad semantics to use here
-        CreateBAT(msg.sender, tokens); // logs token creation
-      }
+
+      // return money if something goes wrong
+      if (tokenCreationCap < checkedSupply) throw;  // odd fractions won't be found
+
+      totalSupply = checkedSupply;
+      balances[msg.sender] += tokens;  // safeAdd not needed; bad semantics to use here
+      CreateBAT(msg.sender, tokens);  // logs token creation
     }
 
     /// @dev Ends the funding period and sends the ETH home
